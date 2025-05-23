09d6ab20367b32c4de1dd37e255785b85607f317
Jimmy Chan <jimmy040825@gmail.com>
2025-05-23 17:46:50 +1000

    Initial commit

---
+++ SomeLibDemo/cpp/adder.cpp
@@ -0,0 +1,6 @@
+#include "adder.h"
+
+int mearlymath::add(int a, int b)
+{
+    return a + b;
+}
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "SomeLibDemo/cpp/adder.h"
+
+int main()
+{
+    std::cout << "2 + 3 = " << mearlymath::add(2, 3) << std::endl;
+    return 0;
+}
